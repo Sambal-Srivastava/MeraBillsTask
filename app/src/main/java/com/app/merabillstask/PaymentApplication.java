94a794d63982fabae8b89e8b4c411f372c49ca77
sambal-srivastava <sambalsrivastava@gmail.com>
2025-03-13 18:19:51 +0530

    created project structure, added DI- HILT

---
+++ app/src/main/java/com/app/merabillstask/PaymentApplication.java
@@ -0,0 +1,9 @@
+package com.app.merabillstask;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class PaymentApplication extends Application {
+}
